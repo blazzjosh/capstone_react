cf9b75c5b030f507113e934583f0b1c799fa2059
cradlydev <blazzjosh@live.com>
2023-11-02 10:39:39 -0400

    tests

--- src/Components/ReportsLayout/ReportsLayout.js
+++ src/Components/ReportsLayout/ReportsLayout.js
@@ -3,8 +3,8 @@ import './ReportsLayout.css';
 
 const ReportLayout = () => {
     const data = [
-        { id: 1, name: 'Dr. John Doe', speciality: 'Cardiology' },
-        { id: 2, name: 'Dr. Smith', speciality: 'Dermatology' }
+        // { id: 1, name: 'Dr. John Doe', speciality: 'Cardiology' },
+        // { id: 2, name: 'Dr. Smith', speciality: 'Dermatology' }
     ];
 
     const viewReport = () => {
@@ -18,28 +18,32 @@ const ReportLayout = () => {
     return (
         <div className="table-container">
             <h1>Reports</h1>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Serial Number</th>
-                        <th>Doctor Name</th>
-                        <th>Doctor Speciality</th>
-                        <th>View Report</th>
-                        <th>Download Report</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {data.map(row => (
-                        <tr key={row.id}>
-                            <td>{row.id}</td>
-                            <td>{row.name}</td>
-                            <td>{row.speciality}</td>
-                            <td><button onClick={() => viewReport()}>View Report</button></td>
-                            <td><button onClick={() => downloadReport()}>Download Report</button></td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+            {data.length > 0 ? (
+                 <table>
+                 <thead>
+                     <tr>
+                         <th>Serial Number</th>
+                         <th>Doctor Name</th>
+                         <th>Doctor Speciality</th>
+                         <th>View Report</th>
+                         <th>Download Report</th>
+                     </tr>
+                 </thead>
+                 <tbody>
+                     {data.map(row => (
+                         <tr key={row.id}>
+                             <td>{row.id}</td>
+                             <td>{row.name}</td>
+                             <td>{row.speciality}</td>
+                             <td><button onClick={() => viewReport()}>View Report</button></td>
+                             <td><button onClick={() => downloadReport()}>Download Report</button></td>
+                         </tr>
+                     ))}
+                 </tbody>
+             </table>
+            ) : (
+                <p>You do not have any reports available.</p>
+            )} 
         </div>
     );
 };
